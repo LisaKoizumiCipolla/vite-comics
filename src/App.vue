d564fefde8918fa52cbe84fa32578217a7313f91
LisaKoizumiCipolla <cipollalisa17@gmail.com>
2023-06-15 13:28:03 +0200

    Create base

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import NewHeader from './components/NewHeader.vue';
+import NewMain from './components/NewMain.vue';
+import NewFooter from './components/NewFooter.vue';
+import NewHeader from './components/NewHeader.vue';
+import NewMain from './components/NewMain.vue';
+import NewFooter from './components/NewFooter.vue';
+
+export default {
+    name: 'App',
+    components:{
+      NewHeader,
+      NewMain,
+      NewFooter
+    }
+}
+</script>
+
+<template>
+  <NewHeader />
+  <NewMain />
+  <NewFooter />
+</template>
+
+<style lang="scss">
+@import "./styles/general.scss";
+</style>
