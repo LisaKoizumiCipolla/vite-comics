896133163eacb9e9459a0b805cb05f7220dcd82d
LisaKoizumiCipolla <cipollalisa17@gmail.com>
2023-06-16 00:46:01 +0200

    Bonus complete

--- src/components/NewMain.vue
+++ src/components/NewMain.vue
@@ -1,7 +1,32 @@
 <template>
     <main>
         <div class="main-section"></div>
-        <div class="bonus-section"></div>
+        <div class="bonus-section flex">
+            <div class="wrapper">
+                <ul class="flex">
+                    <li>
+                        <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+                        <span>DIGITAL COMICS</span>
+                    </li>
+                    <li>
+                        <img src="../assets/img/buy-comics-merchandise.png" alt="">
+                        <span>DC MERCHANDISE</span>
+                    </li>
+                    <li>
+                        <img src="../assets/img/buy-comics-subscriptions.png" alt="">
+                        <span>SUBSCRIPTION</span>
+                    </li>
+                    <li>
+                        <img src="../assets/img/buy-comics-shop-locator.png" alt="">
+                        <span>COMIC SHOP LOCATOR</span>
+                    </li>
+                    <li>
+                        <img class="svg" src="../assets/img/buy-dc-power-visa.svg" alt="">
+                        <span>DC POWER VISA</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </main>
 </template>
 
@@ -16,6 +41,8 @@ export default{
 
 <style lang="scss" scoped>
 
+    @use '../styles/partials/mixins' as *;
+    @use '../styles/partials/variables' as *;
     .main-section{
         height: 130px;
         background-image:url(../assets/img/jumbotron.jpg);
@@ -25,5 +52,31 @@ export default{
     .bonus-section{
         height: 150px;
         background-color: #0282F9;
+        @include flex(row, center, center);
+    }
+
+    .wrapper{
+        margin: auto;
+    }
+
+    ul{
+    @include flex(row, space-around, center);
+
+        li{
+            display: flex;
+            align-items: center;
+            color: white;
+            font-size: .95rem;
+            font-weight: 400;
+
+            img{
+                height: 55px;
+                margin-right: 20px;
+            }
+
+            .svg{
+                height: 35px;
+            }
+        }
     }
 </style>
